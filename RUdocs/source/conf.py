8a868aa96499a529d8c97d828398ed7aebdb9da4
MaslovaEV <maslova_ev@bw-sw.com>
2018-05-17 15:18:31 +0700

    Update conf.py

--- RUdocs/source/conf.py
+++ RUdocs/source/conf.py
@@ -88,7 +88,7 @@ todo_include_todos = True
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'alabaster'
+html_theme = 'default'
 
 html_logo = 'cloudstack_logo_light.png'
 # Theme options are theme-specific and customize the look and feel of a theme
